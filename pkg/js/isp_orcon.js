9339e2f9cf6a63ab0b4a8fb4f248856981fc0d82
lukerossrobinson <lukerossrobinson@ea8e0c11-3fac-c418-2ef1-775f157eb8db>
2010-07-21 04:34:47 +0000

    updated so Orcon works with refactored code

--- pkg/js/isp_orcon.js
+++ pkg/js/isp_orcon.js
@@ -5,6 +5,7 @@ MUST implement the following methods:
 getCustomOptions (returns an array of Option Objects)
 getConnectionDetails (returns a ConnectionDetails Object)
 processData(xml, text) (processes the supplied input data to return a UsageData Object)
+getUsageTypes() (returns the different Usage Types available in this ISPs data)
 
 See util.js for details on individual Objects
 */
@@ -41,28 +42,14 @@ function processData(xml, text) {
 	data.plan = result[1];
 	data.unit = "GB";
 	
-	data.peakQuota = localStorage["quota"];
-//	data.offpeakQuota = data.peakQuota;
-	data.uploadQuota = data.peakQuota;
-	data.miscQuota = data.peakQuota;
+	data.usageTypes["Total Usage"] = new UsageType();
+	data.usageTypes["Total Downloads"] = new UsageType();
+	data.usageTypes["Total Uploads"] = new UsageType();
 	
-	var peakDlRegex = /<dt>Total Downloads<\/dt>\s*<dd>([\d\.]+?) GB<\/dd>/;
-	result = peakDlRegex.exec(text);
-	if (result == null) {
-		data.loaded = false;
-		data.error = "Download information not found";
-		return data;
-	}
-	data.peakDl = result[1];
-//	data.offpeakDl;
-	var uploadRegex = /<dt>Total Uploads<\/dt>\s*<dd>([\d\.]+?) GB<\/dd>/;
-	result = uploadRegex.exec(text);
-	if (result == null) {
-		data.loaded = false;
-		data.error = "Upload information not found";
-		return data;
-	}
-	data.upload = result[1];
+	var quota = localStorage["quota"];
+	data.usageTypes["Total Usage"].quota = quota;
+	data.usageTypes["Total Downloads"].quota = quota;
+	data.usageTypes["Total Uploads"].quota = quota;
 	
 	var totalUsageRegex = /<dt>Total Usage<\/dt>\s*<dd>([\d\.]+?) GB<\/dd>/;
 	result = totalUsageRegex.exec(text);
@@ -71,8 +58,25 @@ function processData(xml, text) {
 		data.error = "Total Usage information not found";
 		return data;
 	}
-	data.miscUsage = result[1];
-	data.miscName = "Total";
+	data.usageTypes["Total Usage"].usage = result[1];
+	
+	var peakDlRegex = /<dt>Total Downloads<\/dt>\s*<dd>([\d\.]+?) GB<\/dd>/;
+	result = peakDlRegex.exec(text);
+	if (result == null) {
+		data.loaded = false;
+		data.error = "Download information not found";
+		return data;
+	}
+	data.usageTypes["Total Downloads"].usage = result[1];
+
+	var uploadRegex = /<dt>Total Uploads<\/dt>\s*<dd>([\d\.]+?) GB<\/dd>/;
+	result = uploadRegex.exec(text);
+	if (result == null) {
+		data.loaded = false;
+		data.error = "Upload information not found";
+		return data;
+	}
+	data.usageTypes["Total Uploads"].usage = result[1];
 	
 	var datesRegex = /<dt>Period<\/dt>\s*<dd>([\d\/]+?) - ([\d\/]+?)<\/dd>/;
 	result = datesRegex.exec(text);
@@ -91,6 +95,10 @@ function processData(xml, text) {
 	
 	data.loaded = true;
 	return data;
+}
+
+function getUsageTypes() {
+	return [["Total Usage","Total Usage"], ["Total Downloads","Total Downloads"], ["Total Uploads","Total Uploads"]];
 }
 
 //Parses the dateString in the format dd/MM/yyyy
