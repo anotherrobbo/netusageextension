b208dc4d29cbb8f6b9edc3054f3ee870d938a9d8
lukerossrobinson <lukerossrobinson@ea8e0c11-3fac-c418-2ef1-775f157eb8db>
2014-10-02 09:58:08 +0000

    Bring everything back up to speed

--- trunk/pkg/js/isp_adam.js
+++ trunk/pkg/js/isp_adam.js
@@ -14,64 +14,64 @@ function getCustomOptions() {
 	// make a call to function in util.js
 	var options = getBasicOptions();
 	var count = options.length;
-	options[count++] = new Option("input", "Username", "username");
-	options[count++] = new Option("input", "Password", "password");
+	options[count++] = new Option("input", "Token", "token");
 	return options;
 }
 
 function getConnectionDetails() {
 	var details = new ConnectionDetails();
 	details.action = "GET";
-	username = localStorage["username"];
-	password = localStorage["password"];
-	details.url = debug ? "adam.xml" : "https://" + username + ":" + password + "@members.adam.com.au/um2.1/usage.php";
-	details.loaded = username && password;
+	token = localStorage["token"];
+	details.url = debug ? "adam.xml" : "https://:" + token + "@members.adam.com.au/api/";
+	details.loaded = token;
 	if (!details.loaded) {
-		details.error = "Username or Password missing"
+		details.error = "Token missing"
 	}
 	return details;
 }
 
-function processData(xml, text) {
-	var accounts = xml.getElementsByTagName("Account");
+function getXml(xml, tagName, attrName, attrValue) {
+	var items = xml.getElementsByTagName(tagName);
 
-	for (var i = 0, account; account = accounts[i]; i++) {
-		var type = account.getAttribute("type");
-		if ("ADSL" == type) {
-			return loadAccount(account);
+	for (var i = 0, item; item = items[i]; i++) {
+		var type = item.getAttribute(attrName);
+		if (attrValue == type) {
+			return item;
 		}
 	}	
 }
 
+function processData(xml, text) {
+	account = getXml(xml, "Account", "type", "ADSL");
+	return loadAccount(account);
+}
+
 function getUsageTypes() {
-	return [["Peak DL","Peak DL"], ["OffPeak DL","OffPeak DL"], ["Upload","Upload"], ["Newsgroup","Newsgroup"]];
+	return [["Download","Download"], ["Newsgroup","Newsgroup"]];
 }
 
 function loadAccount(account) {
 	var data = new UsageData();
 	data.user = account.getAttribute("username");
+	var planName = account.getElementsByTagName("PlanName")[0].textContent;
 	var planType = account.getElementsByTagName("PlanType")[0].textContent;
 	var planSpeed = account.getElementsByTagName("PlanSpeed")[0].textContent;
-	data.plan = planType + " " + planSpeed;
-	data.unit = "MB";
+	data.plan = planName;// + "<br/>" + planType + " " + planSpeed;
+	data.unit = "B";
 	
-	data.usageTypes["Peak DL"] = new UsageType();
-	data.usageTypes["OffPeak DL"] = new UsageType();
-	data.usageTypes["Upload"] = new UsageType();
+	data.usageTypes["Download"] = new UsageType();
 	data.usageTypes["Newsgroup"] = new UsageType();
 	
-	var quota = account.getElementsByTagName("MegabyteQuota")[0].textContent;
-	var datablocks = account.getElementsByTagName("MegabyteDatablocks")[0].textContent;
-	data.usageTypes["Peak DL"].quota = parseInt(quota) + parseInt(datablocks);
-	data.usageTypes["OffPeak DL"].quota = data.usageTypes["Peak DL"].quota;
-	data.usageTypes["Upload"].quota = data.usageTypes["Peak DL"].quota;
-	data.usageTypes["Newsgroup"].quota = account.getElementsByTagName("NewsgroupQuota")[0].textContent;
+	var dloadBucket = getXml(account, "Bucket", "desc", "Download");
+	var newsgroupBucket = getXml(account, "Bucket", "desc", "Newsgroups");
+
+	var quota = dloadBucket.getElementsByTagName("Quota")[0].textContent;
+	var datablocks = account.getElementsByTagName("Datablocks")[0].textContent;
+	data.usageTypes["Download"].quota = parseInt(quota) + parseInt(datablocks);
+	data.usageTypes["Newsgroup"].quota = newsgroupBucket.getElementsByTagName("Quota")[0].textContent;
 	
-	var usage = account.getElementsByTagName("Usage")[0];
-	data.usageTypes["Peak DL"].usage = usage.getElementsByTagName("MegabytesDownloadedPeak")[0].textContent;
-	data.usageTypes["OffPeak DL"].usage = usage.getElementsByTagName("MegabytesDownloadedOffPeak")[0].textContent;
-	data.usageTypes["Upload"].usage = usage.getElementsByTagName("MegabytesUploadedTotal")[0].textContent;
-	data.usageTypes["Newsgroup"].usage = usage.getElementsByTagName("MegabytesNewsgroupTotal")[0].textContent;
+	data.usageTypes["Download"].usage = dloadBucket.getElementsByTagName("Usage")[0].textContent;
+	data.usageTypes["Newsgroup"].usage = newsgroupBucket.getElementsByTagName("Usage")[0].textContent;
 	
 	var lastResetDate = parseAdamDate(account.getElementsByTagName("QuotaStartDate")[0].textContent);
 	data.lastReset = formatDate(lastResetDate);
